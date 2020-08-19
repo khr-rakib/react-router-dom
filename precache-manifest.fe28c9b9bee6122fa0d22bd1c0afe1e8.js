0526180826ebe5ae68db0a4108c061449627fb15
khr-rakib <khr.rakib@gmail.com>
2020-08-19 06:07:44 +0600

    Updates

---
+++ precache-manifest.fe28c9b9bee6122fa0d22bd1c0afe1e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39b6c3b6a22ffc287421f4fcbc76c93",
+    "url": "/react-router-dom/index.html"
+  },
+  {
+    "revision": "19ba0c3fc49bdf2d3bf3",
+    "url": "/react-router-dom/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3ae37dbb29ee8c8ddbd5",
+    "url": "/react-router-dom/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "19ba0c3fc49bdf2d3bf3",
+    "url": "/react-router-dom/static/js/2.2c812042.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router-dom/static/js/2.2c812042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae37dbb29ee8c8ddbd5",
+    "url": "/react-router-dom/static/js/main.8a9a0d62.chunk.js"
+  },
+  {
+    "revision": "2569d54242d4473201bb",
+    "url": "/react-router-dom/static/js/runtime-main.f98ff7ea.js"
+  }
+]);
